33575243737a29816b523ad0da0bedc98ea0606c
Sodiq Ganiyu <gsolaniyi@gmail.com>
2024-02-06 11:42:24 +0100

    complete the functionanlity

--- src/App.jsx
+++ src/App.jsx
@@ -5,13 +5,39 @@ import "./App.css";
 
 function App() {
   const [questions, setQuestions] = useState([
-    { id: 1, questions: "what is access", answer: "Access is a bank" },
-    { id: 2, questions: "what is First", answer: "Access is a First bank" },
-    { id: 3, questions: "what is Diamond", answer: "Access is a diamond bank" },
+    { id: 1, question: "what is access", answer: "Access is a bank" },
+    { id: 2, question: "what is First", answer: "Access is a First bank" },
+    { id: 3, question: "what is Diamond", answer: "Access is a diamond bank" },
   ]);
+
   return (
     <>
-      <h1>Hello Sodiq</h1>
+      {/* INTRO SECTION */}
+      <h1>🃏 Fash Card NG 🧠</h1>
+      <h2>why flash card intro</h2>
+      <h3>why flash more intro</h3>
+
+      {/* FORM SECTION */}
+      <form className="question-form">
+        <label>
+          Question <br></br>
+          <input />
+          <br></br>
+        </label>
+        <label>
+          Answer <br></br>
+          <input />
+        </label>
+      </form>
+
+      {/* QNA DISPLAY SECTION */}
+      <div className="qbox-container">
+        {questions.map((q) => (
+          <div key={q.id} className="q-box">
+            {q.question}
+          </div>
+        ))}
+      </div>
     </>
   );
 }
